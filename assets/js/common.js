bd55039af05f942821e2a8fa081663a501e3e8a0
thethsuqualy <thethsukhin.qualymm@gmail.com>
2025-06-11 10:30:12 +0630

    scroll

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,21 @@
 // Your JavaScript code goes here
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const topBtn = document.querySelector('.top-btn');
+
+        // Show or hide the button when scrolling
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 200) {
+                topBtn.classList.remove('nottop');
+            } else {
+                topBtn.classList.add('nottop');
+            }
+        });
+
+        // Scroll to top when the button is clicked
+        topBtn.addEventListener('click', () => {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+});
 
 // Your jQuery code goes here
 $(function () {
@@ -14,25 +30,26 @@ $(function () {
     }
   });
 
-  // $(".companies").slick({
-  //   slidesToShow: 3,
-  //   infinite: true,
-  //   slidesToScroll: 1,
-  //   centerMode: true,
-  //   autoplay: false,
-  //   autoplaySpeed: 2000,
-  // });
+  $(".test-wrapper").slick({
+    slidesToShow: 1,
+    infinite: true,
+    slidesToScroll: 1,
+    centerMode: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 
    $('.companies').slick({
-            slidesToShow: 3,
+            slidesToShow: 4,
             infinite: true,
             slidesToScroll: 1,
-            centerMode: true,
-            autoplay:false,
+            centerMode: false,
+            autoplay:true,
             autoplaySpeed:2000,
-            dots: true,
-            // draggable: true,
+            // dots: true,
+            draggable: true,
             // initialSlide: 2,
-            variableWidth: true,
+            
         });
+  
 });
